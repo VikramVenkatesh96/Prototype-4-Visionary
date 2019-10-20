960909bcac1ebd782203f668375c32b6a488c8dc
Vikram Venkatesh <venkateshvikram96@gmail.com>
2019-10-19 22:46:49 -0600

    Added Editor Scripts for Generation of various items
    Moving in the direction of a procedural workflow

---
+++ VRProjectNew/Assets/Editor/GenCerealRowEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GenCerealRow))]
+public class GenCerealRowEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        GenCerealRow generator = (GenCerealRow)target;
+        if (DrawDefaultInspector()) {
+            if (generator.autoUpdate) {
+                if (GameObject.FindGameObjectWithTag("Items") != null)
+                {
+                    generator.DestroyCereal();
+                }
+                generator.GenerateCereal();
+            }
+        }
+        if (GUILayout.Button("Generate!")) {
+            if (GameObject.FindGameObjectWithTag("Items") != null)
+            {
+                generator.DestroyCereal();
+            }
+            generator.GenerateCereal();
+        }
+    }
+}
