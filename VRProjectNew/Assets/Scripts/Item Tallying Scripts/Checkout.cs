19b88d549bba0274edc8ea82a24d4b8bbb8f8a84
Azhar Siddiqui <ozzie4391@gmail.com>
2019-10-22 13:27:34 -0600

    Added logic for checking picked items against shopping list

--- VRProjectNew/Assets/Scripts/Item Tallying Scripts/Checkout.cs
+++ VRProjectNew/Assets/Scripts/Item Tallying Scripts/Checkout.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Checkout : MonoBehaviour
 {
     private CheckItems check;
+    public List<GameObject> shoppingList;
 
     void Start()
     {
+        shoppingList = new List<GameObject>();
+
+        //shoppingList.Add(GameObject.FindGameObjectWithTag("Items"));
+
         //a little manual effort but saves a lot of computations
         check = GameObject.FindGameObjectWithTag("Basket").transform.GetChild(5).GetComponent<CheckItems>();
     }
@@ -18,6 +23,14 @@ public class Checkout : MonoBehaviour
         List<Collider> basketItems = check.GetBasketItems();
         //Print them
         foreach (Collider item in basketItems) {
+            foreach(GameObject item1 in shoppingList)
+            {
+                if (item == item1)
+                {
+                    print("Correct Item picked!");
+                    break;
+                }
+            }
             Debug.Log(item.name);
         }
     }
