f69d5840f38bf8e0e24f12f034c277d768968160
Vikram Venkatesh <venkateshvikram96@gmail.com>
2019-10-27 18:27:10 -0600

    Finished first version of generation code
    Added support for rotation

--- VRProjectNew/Assets/Scripts/Generation Scripts/GenRow.cs
+++ VRProjectNew/Assets/Scripts/Generation Scripts/GenRow.cs
@@ -4,48 +4,82 @@ using UnityEngine;
 
 public class GenRow : MonoBehaviour
 {
-    public int MAXNUMBER = 3;
-    public bool autoUpdate;
-    public GameObject parent;
+
+    //temporary arrangement to stop ShoppingListGenerator from throwing errors
     public GameObject[] stocks;
-    public List<Vector3> startPositions;
+    public int maxNumber = 3;
+    public bool autoUpdate;
     public float minGapBetweenObjects;
     public float maxGapBetweenObjects;
+    private List<Vector3> startPositions;
+    private List<Vector3> endPositions;
+    private GameObject[] Shelves;
+    private Warehouse wareHouse;
     
     //Currently works only for those shelves with default rotation
     public void GenerateItems() {
 
-        List<float> offsets = new List<float>();
+        startPositions = new List<Vector3>();
+        endPositions = new List<Vector3>();
+        Shelves = GameObject.FindGameObjectsWithTag("Shelf");
+        wareHouse = gameObject.GetComponentInChildren<Warehouse>();
 
-        if (startPositions.Count != stocks.Length)
-        {   //Clear the start positions list
-            startPositions.Clear();
-            //Then add the required vectors
-            for (int i = 0; i < stocks.Length; ++i)
-            {
-                startPositions.Add(new Vector3(0, 0, 0));
-            }
-        }
-
-        // Generate all the items in the stocks
-        for(int i = 0; i < stocks.Length; ++i)
+        foreach (GameObject shelf in Shelves)
         {
-            offsets.Add(startPositions[i].z);
-            GameObject stock = stocks[i];
-            for (int j = 0; j < MAXNUMBER; ++j)
+            //store the startPositions and endPositions in a list for later ease of calculations 
+            int numOfRows = shelf.transform.GetChild(1).childCount;
+            
+            //Store startPositions and endPositions for the current shelf
+            for (int i = 0; i < numOfRows; ++i)
             {
-                Vector3 position = new Vector3(startPositions[i].x, startPositions[i].y, offsets[i]);
-                GameObject newItem = Instantiate(stock, position, stock.transform.rotation, parent.transform);
-                newItem.name = stock.name + " " + j;
-                offsets[i] -= Random.Range(minGapBetweenObjects, maxGapBetweenObjects);
+                //Debug.Log()
+                startPositions.Add(shelf.transform.GetChild(1).GetChild(i).transform.position);
+                endPositions.Add(shelf.transform.GetChild(2).GetChild(i).transform.position);
+
             }
+
+            //Set the stock for that particular shelf
+            Shelf shelfComponent = shelf.GetComponent<Shelf>();
+            
+            //Currently configured for cereals. To add: mechanism to control for other type of objects
+            shelfComponent.stock = wareHouse.GetRandomCereals(numOfRows);
+
+            //Find parent items GameObject
+            GameObject parent = shelf.transform.Find("Items").gameObject;
+
+            //Populate the Shelf for each row in the shelf
+            for (int i = 0; i < numOfRows; ++i)
+            {
+                PopulateShelf(i, shelfComponent.stock[i] ,parent);
+            }
+
+            //Clear all lists before adding again
+            startPositions.Clear();
+            endPositions.Clear();
+
         }
+
     }
 
     public void DestroyItems() {
+
         GameObject[] items = GameObject.FindGameObjectsWithTag("Items");
         foreach (GameObject item in items) {
             DestroyImmediate(item);
         }
     }
+
+    private void PopulateShelf(int rowNumber, GameObject stock, GameObject parent)
+    {
+        float offset = 0;
+
+        for (int i = 0; i < maxNumber; ++i)
+        {
+            Vector3 newPos = startPositions[rowNumber] + (endPositions[rowNumber] - startPositions[rowNumber]).normalized * offset;
+            Instantiate(stock, newPos, parent.transform.rotation, parent.transform);
+            offset += Random.Range(minGapBetweenObjects, maxGapBetweenObjects);
+        }
+    }
+
 }
+
---
+++ VRProjectNew/Assets/Scripts/Generation Scripts/Warehouse.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warehouse : MonoBehaviour
+{
+    public GameObject[] cereals;
+    public GameObject[] milk;
+    public GameObject[] cans;
+
+    public GameObject[] GetRandomCereals(int numOfRows) {
+        List<GameObject> randomCereals = new List<GameObject>();
+
+        while (randomCereals.Count != numOfRows ){ 
+            int index = Random.Range(0,cereals.Length - 1);
+            if (!randomCereals.Contains(cereals[index]))
+            {
+                randomCereals.Add(cereals[index]);
+            }
+        }
+
+        return randomCereals.ToArray();
+    }
+    public GameObject[] GetRandomMilk(int numOfRows)
+    {
+        GameObject[] randomMilk = new GameObject[numOfRows];
+
+        for (int i = 0; i < numOfRows; ++i)
+        {
+            int index = Random.Range(0, milk.Length - 1);
+            randomMilk[i] = milk[index];
+        }
+
+        return randomMilk;
+    }
+    public GameObject[] GetRandomCans(int numOfRows)
+    {
+        GameObject[] randomCans = new GameObject[numOfRows];
+
+        for (int i = 0; i < numOfRows; ++i)
+        {
+            int index = Random.Range(0, cans.Length - 1);
+            randomCans[i] = cans[index];
+        }
+
+        return randomCans;
+    }
+
+}
